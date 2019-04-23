4fdec7c98d4bc7f02f9966716ddd0c047d4dc8ed
dhairya.chheda <dhairya.chheda@somaiya.edu>
2019-04-23 21:14:46 +0530

    Added OnClickListener to the button in Main Activity.

--- app/src/main/java/com/dccorp/auditcourse/MainActivity.java
+++ app/src/main/java/com/dccorp/auditcourse/MainActivity.java
@@ -1,13 +1,65 @@
 package com.dccorp.auditcourse;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telecom.ConnectionRequest;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+
+    private Button goToMaps = (Button)findViewById(R.id.btn);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(isServicesOK()){
+            init();
+        }
+    }
+    private void init(){
+        goToMaps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public boolean isServicesOK(){
+        Log.d(TAG, "isServicesOK: checking google services version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+
+        if(available == ConnectionResult.SUCCESS){
+            //everything is working fine :)
+            Log.d(TAG, "isServicesOK: Google Play Services is working properly");
+            return true;
+        }
+
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            //a resolvable error occured
+            Log.d(TAG, "isServicesOK: An error occured but it can be fixed");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }
+        else {
+            Toast.makeText(this,"We can't make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 }
